cb68ebe4c9e76721509c1f209e9228028af7396b
salexzee <510samwebb@gmail.com>
2021-12-12 20:32:46 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Shopify Cart Scripts
+
+A bunch of scripts for merchants to use in relation to Inveterate in the Shopify Script Editor app.
+
+[Script Editor App](https://apps.shopify.com/script-editor)
+[Script Editor Docs](https://help.shopify.com/en/manual/checkout-settings/script-editor/shopify-scripts)
---
+++ scripts/shipping/discounted_shipping.rb
@@ -0,0 +1,51 @@
+# Editable Values
+PERCENT_OFF = 100
+SHIPPING_CODES = [
+  "Economy",
+  "Standard",
+  "Express"
+]
+MESSAGE = "Free shipping for subscribers!"
+
+########
+# DO NOT EDIT PAST THIS POINT
+########
+
+class InveterateFreeShipping 
+  def initialize()
+    @percent_off = PERCENT_OFF * 0.01
+    @shipping_codes = SHIPPING_CODES
+    @message = MESSAGE
+  end
+
+  def run(shipping_rates, cart)
+    @shipping_rates = shipping_rates
+    @cart = cart
+    start
+  end
+
+  private
+  
+  def start
+    @shipping_rates.each do |shipping_rate|
+      next unless @cart.customer
+      next unless @cart.customer.tags.include? "inveterate-subscriber"
+      next unless shipping_rate.source == "shopify"
+      next unless @shipping_codes.include? shipping_rate.code
+      shipping_rate.apply_discount(
+        shipping_rate.price * @percent_off,
+        message: @message
+      )
+    end
+  end
+end
+
+CAMPAIGNS = [
+  InveterateFreeShipping.new()
+]
+
+CAMPAIGNS.each do |campaign|
+  campaign.run(Input.shipping_rates, Input.cart)
+end
+
+Output.shipping_rates = Input.shipping_rates
